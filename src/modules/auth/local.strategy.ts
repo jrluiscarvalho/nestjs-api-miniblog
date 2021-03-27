58c12cf8b3c4b71b0dcac10abc6678cbe590d059
luiscarvalho17 <jrluiscarvalho@gmail.com>
2021-03-27 19:29:52 -0300

    create initials configurations from auth module

---
+++ src/modules/auth/local.strategy.ts
@@ -0,0 +1,18 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport';
+
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super();
+  }
+
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid user credentials');
+    }
+    return user;
+  }
+}
